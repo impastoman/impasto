9a829ccfad24d060394a4e9a01e7c9e248a23150
impastoman <wy.sayo@gmail.com>
2026-06-18 13:22:43 -0700

    Resolve merge conflict, clean up ImpastoStyle remnant

---
+++ Impasto/Impasto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Impasto
+//
+//  Created by William Sayo on 5/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Impasto/Impasto/ImpastoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImpastoApp.swift
+//  Impasto
+//
+//  Created by William Sayo on 5/10/26.
+//
+
+import SwiftUI
+
+@main
+struct ImpastoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
